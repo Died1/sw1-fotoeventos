098c9bf46e7164ac3959c12f57ef02554cfc02de
Eddy F_____ <eddycampos2011@gmail.com>
2024-01-30 02:13:15 -0400

    no hay

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -101,7 +101,7 @@ class EventController extends Controller
                 $event->update();
                 return $event;
             }
-            return response()->json(['message' => $foto], 404);
+            return response()->json(['message' => 'no hay'], 404);
 
         } catch (\Exception $e) {
             //throw $th;
