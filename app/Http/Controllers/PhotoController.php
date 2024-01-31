29d79e6c2fdc118cf8212d9e7d25f0b119b65976
Eddy F_____ <eddycampos2011@gmail.com>
2024-01-31 15:08:38 -0400

    ffoto actualizado

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -17,7 +17,26 @@ class PhotoController extends Controller
             if ($file) {
 
                 $image_path = Storage::disk('s3')->put("events/$eventID/photos", $file);
-                $urlFoto = env('AWS_BUCKET_URL') . $image_path;
+                $urlFotoOriginal = env('AWS_BUCKET_URL') . $image_path;
+
+                $image = Image::make($file);
+                $image->fit(250, 200); // Redimensionar la foto a 250x200 manteniendo la proporción
+
+                // Guardar la foto redimensionada en S3
+                $imagePathRedimensionada = "events/{$eventID}/photos/" . uniqid() . '.jpg';
+                Storage::disk('s3')->put($imagePathRedimensionada, $image->stream()->__toString());
+                $urlFotoRedimensionada = env('AWS_BUCKET_URL') . $imagePathRedimensionada;
+
+                // Guardar la URL de ambas fotos en la base de datos
+                $photoOriginal = new Photo();
+                $photoOriginal->url_preview = $urlFotoOriginal;
+                $photoOriginal->event_id = $eventID;
+                $photoOriginal->save();
+
+                $photoRedimensionada = new Photo();
+                $photoRedimensionada->url_preview = $urlFotoRedimensionada;
+                $photoRedimensionada->event_id = $eventID;
+                $photoRedimensionada->save();
 
                 /* // Redimensionar la imagen a 300x250
                 $image = Image::make(storage_path('app/public/' . $path));
@@ -29,10 +48,10 @@ class PhotoController extends Controller
 
                 $image->save();*/
 
-                $photo = new Photo();
+                /* $photo = new Photo();
                 $photo->url_preview = $urlFoto;
                 $photo->event_id = $eventID;
-                $photo->save();
+                $photo->save(); */
                 return response()->json([
                     'status' => true,
                     'message' => 'File received successfully.',
